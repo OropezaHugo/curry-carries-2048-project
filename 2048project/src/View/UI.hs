4d162dd383ac5c9593123144da1877ecc38c4da1
Jorge.cool <jorge.heredia.13036406@usalesiana.edu.bo>
2024-03-30 23:15:04 -0400

    canvas with lines of the board and one tile of example

--- 2048project/src/View/UI.hs
+++ 2048project/src/View/UI.hs
@@ -15,13 +15,16 @@ startUI = do
         , jsStatic     = Just "../wwwroot"
         } setup
 
+canvasSize = 400
+tileSize = 80.0
+
 setup :: Window -> UI ()
 setup window = do
     _ <- return window # set UI.title "2048 - CurryCarries"
     titleMainPage <- UI.h2 # set UI.text "2048 - The game"
 
-    let imagePath = "2048project\\src\\View\\2048-logo.png"
-    img <- mkElement "img" # set (attr "src") imagePath
+    url <- UI.loadFile "image/png" "C:\\Users\\jorge\\Desktop\\JorgeHB\\Ing.SC\\5toSemestre\\Programming V\\Homework\\curry-carries-2048-project\\2048project\\src\\View\\2048-logo.png"
+    img <- mkElement "img" # set UI.src url # set UI.height 40 # set UI.width 40
 
     instruction1 <- UI.label # set UI.text "1. Merge the blocks with similar value to obtain score."
     instruction2 <- UI.label # set UI.text "2. Obtain the number 2048 to win."
@@ -29,18 +32,38 @@ setup window = do
 
     textColum <- Core.column [element instruction1, element instruction2, element instruction3]
 
-    bestScoreLabel <- UI.label # set UI.text "BestScore: " # set style [("font-family", "'Courier New'"), ("color", "#2F48E0")]
-    bestScore <- UI.label # set UI.text "000: " # set style [("font-family", "'Courier New'"), ("color", "#8080FF")]
-    actualScoreLabel <- UI.label # set UI.text "Score: " # set style [("font-family", "'Courier New'"), ("color", "#2F48E0")]
-    actualScore <- UI.label # set UI.text "000: " # set style [("font-family", "'Courier New'"), ("color", "#8080FF")]
-    rowScore <- Core.row [element bestScoreLabel, element bestScore, element actualScoreLabel, element actualScore] # set style [("display", "flex"), ("align-items", "center")]
+    bestScoreLabel <- UI.label # set UI.text "BestScore: " # set style [("font-family", "'Courier New'"), ("color", "#013D5A")]
+    bestScore <- UI.label # set UI.text "000 " # set style [("font-family", "'Courier New'"), ("color", "#708C69")]
+    actualScoreLabel <- UI.label # set UI.text "Score: " # set style [("font-family", "'Courier New'"), ("color", "#013D5A")]
+    actualScore <- UI.label # set UI.text "000 " # set style [("font-family", "'Courier New'"), ("color", "#708C69")]
+    rowScore <- Core.row [element bestScoreLabel, element bestScore, element actualScoreLabel, element actualScore] # set style [("display", "flex"), ("justify-content", "space-between")]
 
-    --canvas <- UI.canvas # set UI.width 400
-                       -- # set UI.height 400
-    --element canvas # set style [("border", "1px solid black"), ("background-color", "#BBADA0")]
+    canvas <- UI.canvas
+        # set UI.height canvasSize
+        # set UI.width canvasSize
+        # set style [("border", "solid #013D5A 3px"), ("background", "#FCF3E3")]
+    
 
     startGame <- UI.button # set UI.text "Start game"
-    _ <- getBody window #+ [element titleMainPage, element textColum, element img, element startGame, element rowScore]
+    _ <- getBody window #+ [row [element titleMainPage, element img], element textColum, element startGame, element rowScore, element canvas]
+
     on UI.click startGame $ const $ do
-        element startGame # set UI.text "Starting game... wait please"
-    --drawGridLines canvas
+        let lines = [ (100 , 0, 2, 400, "#013D5A")
+                    , (200, 0, 2, 400, "#013D5A")
+                    , (300, 0, 2, 400, "#013D5A")
+                    , (0, 100, 400, 2, "#013D5A")
+                    , (0, 200, 400, 2, "#013D5A")
+                    , (0, 300, 400, 2, "#013D5A")
+                    ]
+
+        forM_ lines $ \(x,y,w,h,color) -> do
+            canvas # set' UI.fillStyle (UI.htmlColor color)
+            canvas # UI.fillRect (x,y) w h
+
+        -- draw a tile
+        canvas # set' UI.fillStyle (UI.htmlColor "#F4A258")
+        return canvas
+            # set UI.textFont    "30px sans-serif"
+            # set UI.strokeStyle "#013D5A"
+        canvas # UI.fillRect (10,10) 80 80
+        canvas # UI.strokeText "2" (40,60)
