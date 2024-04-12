3d6a05a2c597d405a3714b664f10df62984d8e63
aguirrekarina <karina.aguirre.9491656@usalesiana.edu.bo>
2024-04-11 21:46:33 -0400

    eliminate unused functions in save score

--- 2048project/src/SaveHighscore.hs
+++ 2048project/src/SaveHighscore.hs
@@ -1,15 +1,11 @@
 module SaveHighscore (getHighscore, writeNewHighscore) where
     import System.IO
-    import System.IO.Unsafe (unsafePerformIO)
     
     highscorePath :: String
     highscorePath = "src/resources/highscore.txt"
 
     getIOHighScore :: IO String
     getIOHighScore = openFile highscorePath  ReadMode >>= \cont -> hGetContents cont
-
-    getStringHighscore :: String
-    getStringHighscore = unsafePerformIO getIOHighScore
 
     getHighscore :: IO Int
     getHighscore = do
